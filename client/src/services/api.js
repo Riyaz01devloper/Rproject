31bb0dfd644f427db9fa4e39ee1021af415da0e0
Riyaz <riyazmalik2046@gmail.com>
2026-01-14 21:46:22 +0530

    Update API base URL for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://rproject-1-p9k4.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
